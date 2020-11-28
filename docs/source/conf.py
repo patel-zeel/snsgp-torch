7b5ba2be1437320a4beddafc28246943d27085d7
patel-zeel <patel_zeel@iitgn.ac.in>
2020-11-28 20:42:39 +0530

    added docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Zeel B Patel'
 author = 'Zeel B Patel'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
